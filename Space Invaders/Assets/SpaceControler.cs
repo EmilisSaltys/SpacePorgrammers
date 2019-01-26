de5fc37640b8060bc9f42b7261452119515f0eec
EmilisSaltys <47056599+EmilisSaltys@users.noreply.github.com>
2019-01-26 15:52:34 +0200

    Space-Programmers

---
+++ Space Invaders/Assets/SpaceControler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceControler : MonoBehaviour {
+
+    private Vector2 targetpos;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+
+        this.transform.Translate(Input.GetAxis("Horizontal")*0.25f, 0, 0);
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -6f, 6f), -3.8f);
+
+
+
+
+
+    }
+}
